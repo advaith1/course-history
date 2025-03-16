a1c9b2460658db68a642e8d5828874d4c456106e
advaith <advaithj1@gmail.com>
2025-03-15 23:32:42 -0700

    add fall 2025

--- term-data.ts
+++ term-data.ts
@@ -5,6 +5,9 @@ export interface Quarter {
 
 export const termData: Quarter[][] = [
 	[
+		{ id: 2258, name: 'Fall 2025' },
+	],
+	[
 		{ id: 2248, name: 'Fall 2024' },
 		{ id: 2250, name: 'Winter 2025' },
 		{ id: 2252, name: 'Spring 2025' },
