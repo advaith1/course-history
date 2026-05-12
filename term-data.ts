d7e7d386b3402488f11bce78f51fc6c8acfaff10
advaith <advaithj1@gmail.com>
2026-05-11 17:48:13 -0700

    add summer and fall 2026

--- term-data.ts
+++ term-data.ts
@@ -5,9 +5,13 @@ export interface Quarter {
 
 export const termData: Quarter[][] = [
 	[
+		{ id: 2268, name: 'Fall 2026' }
+	],
+	[
 		{ id: 2258, name: 'Fall 2025' },
 		{ id: 2260, name: 'Winter 2026' },
-		{ id: 2262, name: 'Spring 2026' }
+		{ id: 2262, name: 'Spring 2026' },
+		{ id: 2264, name: 'Summer 2026' }
 	],
 	[
 		{ id: 2248, name: 'Fall 2024' },
